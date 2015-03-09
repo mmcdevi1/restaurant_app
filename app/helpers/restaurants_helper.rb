ecf46f6454a0d844800df9d343373eeaef759ad8
Michael McDevitt <mmcdevi1@gmail.com>
2015-03-09 18:14:23 -0400

    edit header

--- app/helpers/restaurants_helper.rb
+++ app/helpers/restaurants_helper.rb
@@ -1,2 +1,18 @@
 module RestaurantsHelper
+
+	# def user_signed_in_links
+	# 	if user_signed_in?
+	# 		unless params[:controller] == "restaurants" && params[:action] == "new"
+	# 			if current_user.role == true
+	# 				%li= link_to "Add New Restaurant", new_restaurant_path
+	# 			end
+	# 		end
+	# 		%li= link_to current_user.name, "#"
+	# 		%li= link_to "Logout", logout_path
+	# 	else
+	# 		%li= link_to "Register", register_path
+	# 		%li= link_to "Login", login_path
+	# 	end
+	# end
+
 end
