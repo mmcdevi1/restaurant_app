dd8e1ad02e4d4855847546edad89fe6fe288018a
Michael McDevitt <mmcdevi1@gmail.com>
2015-03-12 09:17:27 -0400

    gmap

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150308043429) do
+ActiveRecord::Schema.define(version: 20150311184900) do
 
   create_table "restaurants", force: true do |t|
     t.string   "name"
@@ -28,13 +28,12 @@ ActiveRecord::Schema.define(version: 20150308043429) do
 
   create_table "users", force: true do |t|
     t.string   "name"
-    t.boolean  "role",                   default: false
-    t.string   "email",                  default: "",    null: false
-    t.string   "encrypted_password",     default: "",    null: false
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,     null: false
+    t.integer  "sign_in_count",          default: 0,  null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
